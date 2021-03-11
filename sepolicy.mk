68cdd6645db1cd6420d69594d1d0ef2eaa42db71
Stealth1226 <rishawniyer06@gmail.com>
2021-03-11 18:45:12 +0530

    Init Lighthouse

--- sepolicy.mk
+++ sepolicy.mk
@@ -16,5 +16,5 @@ BOARD_PLAT_PRIVATE_SEPOLICY_DIR += \
     device/mediatek/sepolicy/bsp/plat_private \
     device/mediatek/sepolicy/full/plat_private
 
-# Inherit Custom sepolicy for mediatek devices
--include device/custom/sepolicy/mediatek/sepolicy.mk
+# Inherit lighthouse sepolicy for mediatek devices
+-include device/lighthouse/sepolicy/mediatek/sepolicy.mk
